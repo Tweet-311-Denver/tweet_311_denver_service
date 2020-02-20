d41a6a0284bca339747e2944de55e9f7802636b3
Alice Post <alicep2322@gmail.com>
2020-02-19 17:25:37 -0700

    First tests written and passing for get requests

---
+++ test/locations.test.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const Lab = require('@hapi/lab');
+const { expect } = require('@hapi/code');
+const { afterEach, beforeEach, describe, it } = exports.lab = Lab.script();
+const { init } = require('../lib/server');
+
+describe('GET /', () => {
+    let server;
+
+    beforeEach(async () => {
+        server = await init();
+    });
+
+    afterEach(async () => {
+        await server.stop();
+    });
+
+    it('responds with 200', async () => {
+        const res = await server.inject({
+            method: 'get',
+            url: '/'
+        });
+        expect(res.statusCode).to.equal(200);
+        expect(res.payload).to.equal("hello world");
+    });
+});
+
+describe('GET /api/v1/locations', () => {
+    let server;
+
+    beforeEach(async () => {
+        server = await init();
+    });
+
+    afterEach(async () => {
+        await server.stop();
+    });
+
+    it('responds with 200', async () => {
+        const res = await server.inject({
+            method: 'get',
+            url: '/api/v1/locations'
+        });
+
+        expect(res.statusCode).to.equal(200);
+        expect(res.payload.length).to.equal(2);
+    });
+});
