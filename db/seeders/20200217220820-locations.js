0aa6e987b840ce2e9ecb8df62068a91dad8d2074
Alice Post <alicep2322@gmail.com>
2020-02-21 22:56:03 -0700

    Downcase name of locations table

--- db/seeders/20200217220820-locations.js
+++ db/seeders/20200217220820-locations.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Locations', [
+    return queryInterface.bulkInsert('locations', [
       { lat: 39.751129, long: -104.997486, address_desc: "1701 Market St" },
       { lat: 39.752793, long: -104.980223, address_desc: "588-530 25th St, Denver, CO 80205" }
     ], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Locations', null, {});
+    return queryInterface.bulkDelete('locations', null, {});
   }
 };
