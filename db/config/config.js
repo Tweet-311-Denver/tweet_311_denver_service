a7c2e6fb32bb24a9308f370ac16f6bc965119c7f
Alice Post <alicep2322@gmail.com>
2020-02-21 20:58:59 -0700

    Configure production db connection

--- db/config/config.js
+++ db/config/config.js
@@ -15,7 +15,6 @@ module.exports = {
   },
   production: {
     use_env_variable: process.env.DATABASE_URL,
-    url: process.env.DATABASE_URL,
     dialect: "postgres",
     protocol: "postgres"
   }
