4a05f81c198620484ef3507c507c1ca7ec4b5bc3
Alice Post <alicep2322@gmail.com>
2020-02-22 12:20:20 -0700

    Increase precision of lat and long on locations

--- db/migrations/20200222105941_locations.js
+++ db/migrations/20200222105941_locations.js
@@ -3,8 +3,8 @@ exports.up = function(knex) {
   return Promise.all([
     knex.schema.createTable('locations', function(table) {
       table.increments('id').primary();
-      table.decimal('lat');
-      table.decimal('long');
+      table.decimal('lat', 10, 6);
+      table.decimal('long', 10, 6);
       table.string('address_desc');
 
       table.timestamps(true, true);
