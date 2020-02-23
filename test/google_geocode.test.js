6115f9fcdb77c4a43dcc48066ad1f28529c2ec36
Alice Post <alicep2322@gmail.com>
2020-02-23 11:26:57 -0700

    Start creating google_geoservice

---
+++ test/google_geocode.test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const Lab = require('@hapi/lab');
+const { expect } = require('@hapi/code');
+const { afterEach, beforeEach, describe, it } = exports.lab = Lab.script();
+const { init } = require('../lib/server');
+
+describe ("Google Geocode Service")
