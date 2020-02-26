23f58866cc070a557e0d990403efa49e9b8b2db2
Dustin Mikusko <dustinjm713@yahoo.com>
2020-02-26 10:01:23 -0700

    Remove unused Puppeteer functions

--- puppeteer.js
+++ puppeteer.js
@@ -1,44 +1,6 @@
 const puppeteer = require('puppeteer');
 
 exports.methods = {
-  abandonedCarForm: async (options) => {
-    const { property, location, description, email, address } = options;
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-  
-    await page.goto('https://www.denvergov.org/pocketgov/#/report-a-problem', {waitUntil: 'networkidle2'});
-    await page.waitFor(2000);
-    
-    await page.select('#categorySelect', 'REP_ABANDONEDVEHICLE');
-    await page.waitFor(2000);
-    await page.select('#QuestionSelect', `string:${property}`);
-    await page.type('#QuestionText', location, {wait: 100});
-    await page.type('#searchTerm', address, {wait: 100});
-    await page.waitFor(2000);
-    await page.click('#addressInputBtn');
-    await page.type('#description', description, {wait: 100});
-    await page.type('#typedInEmailInput', email, {wait: 100});
-    await page.waitFor(1000);
-    await page.click('#submitReport');
-    await page.waitForNavigation();
-    const confirmationNotes = await page.evaluate(() => {
-      let notes = Array.from(document.querySelectorAll('div.col-sm-9')).map(note => {
-        return note.innerText;
-    });
-      return notes
-    });
-  
-    const confirmation = {
-      caseID: confirmationNotes[0],
-      category: confirmationNotes[1],
-      submittedAs: confirmationNotes[2],
-      submittedAt: confirmationNotes[3],
-      notes: confirmationNotes[4]
-    }
-  
-    await browser.close();
-    return confirmation
-  },
   snowRemoval: async (options) => {
     const { description, email, address } = options
     const browser = await puppeteer.launch();
@@ -74,43 +36,6 @@ exports.methods = {
   
     await browser.close();
     return confirmation
-  },
-  illegalParking: async options => {
-    const { location, description, property, email, address } = options;
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-  
-    await page.goto('https://www.denvergov.org/pocketgov/#/report-a-problem', {waitUntil: 'networkidle2'});
-    await page.waitFor(2000);
-  
-    await page.select('#categorySelect', 'REQ_ILLEGALPARKING');
-    await page.waitForSelector('#QuestionSelect');
-    await page.type('#QuestionText', location, {wait: 100});
-    await page.select('#QuestionSelect', `string:${property}`);
-    await page.type('#searchTerm', address, {wait: 100});
-    await page.waitFor(2000);
-    await page.click('#addressInputBtn');
-    await page.type('#description', description, {wait: 100});
-    await page.type('#typedInEmailInput', email, {wait: 100});
-    await page.waitFor(1000);
-    await page.click('#submitReport');
-    await page.waitForNavigation();
-    const confirmationNotes = await page.evaluate(() => {
-      let notes = Array.from(document.querySelectorAll('div.col-sm-9')).map(note => {
-        return note.innerText;
-    });
-      return notes
-    });
-  
-    const confirmation = {
-      caseID: confirmationNotes[0],
-      category: confirmationNotes[1],
-      submittedAs: confirmationNotes[2],
-      submittedAt: confirmationNotes[3],
-      notes: confirmationNotes[4]
-    }
-    await browser.close();
-    return confirmation;
   },
   otherForm: async options => {
     const { description, email, address } = options;
